b51f384a4d49d77455c4e3af8b848c60908f5376
rcardin <riccardo.cardin@gmail.com>
2021-03-18 21:50:27 +0100

    Added first draft of the ShoppingCartRoutes types

--- src/main/scala/in/rcard/http4s/tutorial/shopping/ShoppingCarts.scala
+++ src/main/scala/in/rcard/http4s/tutorial/shopping/ShoppingCarts.scala
@@ -1,8 +1,15 @@
 package in.rcard.http4s.tutorial.shopping
 
+import cats.Applicative
+import cats.effect.Sync
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import org.http4s.{EntityDecoder, EntityEncoder}
+import org.http4s.circe.{jsonEncoderOf, jsonOf}
+
 trait ShoppingCarts[F[_]] {
   def create(id: ShoppingCarts.ShoppingCartId): F[Unit]
-  def find(id: ShoppingCarts.ShoppingCartId): F[Option[ShoppingCarts.ShoppingCart]]
+  def findBy(id: ShoppingCarts.ShoppingCartId): F[ShoppingCarts.ShoppingCart]
 }
 
 object ShoppingCarts {
@@ -10,11 +17,18 @@ object ShoppingCarts {
   final case class ProductId(id: String) extends AnyVal
   final case class Product(id: ProductId, description: String)
   final case class ShoppingCartId(id: String) extends AnyVal
+
   final case class ShoppingCart(id: ShoppingCartId, products: List[Product])
+  object ShoppingCart {
+    implicit val jokeDecoder: Decoder[ShoppingCart] = deriveDecoder[ShoppingCart]
+    implicit def jokeEntityDecoder[F[_]: Sync]: EntityDecoder[F, ShoppingCart] = jsonOf
+    implicit val jokeEncoder: Encoder[ShoppingCart] = deriveEncoder[ShoppingCart]
+    implicit def jokeEntityEncoder[F[_]: Applicative]: EntityEncoder[F, ShoppingCart] = jsonEncoderOf
+  }
 
   def impl[F[_]]: ShoppingCarts[F] = new ShoppingCarts[F] {
     override def create(id: ShoppingCartId): F[Unit] = ???
 
-    override def find(id: ShoppingCartId): F[Option[ShoppingCart]] = ???
+    override def findBy(id: ShoppingCartId): F[ShoppingCart] = ???
   }
 }
