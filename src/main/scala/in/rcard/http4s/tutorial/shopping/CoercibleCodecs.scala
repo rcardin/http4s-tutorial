a2c9fdbeca9de60a516338282c5ec7fdc8b42756
rcardin <riccardo.cardin@gmail.com>
2021-03-21 15:13:58 +0100

    Made it work with version 0.4.4 of the newtype library

--- src/main/scala/in/rcard/http4s/tutorial/shopping/CoercibleCodecs.scala
+++ src/main/scala/in/rcard/http4s/tutorial/shopping/CoercibleCodecs.scala
@@ -4,17 +4,16 @@ import io.circe._
 import io.estatico.newtype.Coercible
 import io.estatico.newtype.ops._
 
-// In versione 0.4.4 of newtype the repr method was remove :(
 trait CoercibleCodecs {
   implicit def coercibleDecoder[A: Coercible[B, *], B: Decoder]: Decoder[A] =
     Decoder[B].map(_.coerce[A])
 
   implicit def coercibleEncoder[A: Coercible[B, *], B: Encoder]: Encoder[A] =
-    Encoder[B].contramap(_.repr.asInstanceOf[B])
+    Encoder[B].contramap(_.asInstanceOf[B])
 
   implicit def coercibleKeyDecoder[A: Coercible[B, *], B: KeyDecoder]: KeyDecoder[A] =
     KeyDecoder[B].map(_.coerce[A])
 
   implicit def coercibleKeyEncoder[A: Coercible[B, *], B: KeyEncoder]: KeyEncoder[A] =
-    KeyEncoder[B].contramap[A](_.repr.asInstanceOf[B])
+    KeyEncoder[B].contramap[A](_.asInstanceOf[B])
 }
