b4b9f45c239e33d90b669d6fade08b5dacf36b3c
rcardin <riccardo.cardin@gmail.com>
2021-03-23 21:33:11 +0100

    Added the server implementation

--- src/main/scala/in/rcard/http4s/tutorial/Main.scala
+++ src/main/scala/in/rcard/http4s/tutorial/Main.scala
@@ -1,8 +1,9 @@
 package in.rcard.http4s.tutorial
 
 import cats.effect.{ExitCode, IO, IOApp}
+import in.rcard.http4s.tutorial.shopping.ShoppingCartsServer
 
 object Main extends IOApp {
-  def run(args: List[String]) =
-    Http4stutorialServer.stream[IO].compile.drain.as(ExitCode.Success)
+  def run(args: List[String]): IO[ExitCode] =
+    ShoppingCartsServer.stream[IO].compile.drain.as(ExitCode.Success)
 }
--- src/main/scala/in/rcard/http4s/tutorial/shopping/ShoppingCartsServer.scala
+++ src/main/scala/in/rcard/http4s/tutorial/shopping/ShoppingCartsServer.scala
@@ -1,5 +1,23 @@
 package in.rcard.http4s.tutorial.shopping
 
-object ShoppingCartsServer {
+import cats.effect.{ConcurrentEffect, Timer}
+import fs2.Stream
+import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.middleware.Logger
 
+import scala.concurrent.ExecutionContext.global
+
+object ShoppingCartsServer {
+  def stream[F[_]: ConcurrentEffect](implicit T: Timer[F]): Stream[F, Nothing] = {
+    val shoppingCartAlg = ShoppingCarts.impl[F]
+    val httpApp = ShoppingCartRoutes.shoppingCartRoutes[F](shoppingCartAlg).orNotFound
+    val finalHttpApp = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
+    for {
+      exitCode <- BlazeServerBuilder[F](global)
+        .bindHttp(8080, "0.0.0.0")
+        .withHttpApp(finalHttpApp)
+        .serve
+    } yield exitCode
+  }.drain
 }
